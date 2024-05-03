a0e97f9ce17b6bd5046ed4e08f0bae4ef78fd51f
Francisco <146821904+fferrlop@users.noreply.github.com>
2024-05-03 14:42:50 +0200

    He cambiado el diseño

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,5 +5,6 @@ import EstructuraInterfaz.InterfazUsuario;
 public class Main {
     public static void main(String[] args) {
         InterfazUsuario interfaz = new InterfazUsuario();
+        interfaz.mostrarVentana();
     }
 }
